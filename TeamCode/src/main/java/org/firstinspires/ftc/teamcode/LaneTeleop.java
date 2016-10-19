7c8339df4e05d94daf7a78afc5d41585c7775106
Lane Ford <null>
2016-10-19 19:27:36 -0400

    Swapped sticks, changed telemetry welcome message.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaneTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaneTeleop.java
@@ -60,6 +60,7 @@ public class LaneTeleop extends LinearOpMode {
     double          clawOffset      = 0;                       // Servo mid position
     final double    CLAW_SPEED      = 0.02 ;                   // sets rate to move servo
 
+
     @Override
     public void runOpMode() {
         double left;
@@ -72,7 +73,7 @@ public class LaneTeleop extends LinearOpMode {
         robot.init(hardwareMap);
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.addData("Say", "Hello Driver Lane");    //
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -83,8 +84,8 @@ public class LaneTeleop extends LinearOpMode {
 
             // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
             // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
-            right = -gamepad1.left_stick_y;
-            left = -gamepad1.right_stick_y;
+            right = -gamepad1.right_stick_y;
+            left = -gamepad1.left_stick_y;
 
             // Normalize the values so neither exceed +/- 1.0
            /* max = Math.max(Math.abs(left), Math.abs(right));
