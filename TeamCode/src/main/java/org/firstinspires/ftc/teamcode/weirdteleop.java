5add81b47a854024e94e8a5a7ad3f0dce0442d0b
2norwooj <2norwooj@chelsea.k12.mi.us>
2016-11-14 18:38:56 -0500

    added servo option for programming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/weirdteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/weirdteleop.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
